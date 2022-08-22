5a1b316e7b4e94a70c56a35ab888415e7b42601e
Anne Ogongo <anneogongo98@gmail.com>
2022-08-22 15:35:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// const totalBatteries = batteryBatches.reduce(function (accum) {
+//     let total = 0;
+//     for (let i=0; i< batteryBatches.length; i++) {
+//     total += batteryBatches[i];
+//     }
+//     return total 
+// })
+
+
+const totalBatteries = batteryBatches.reduce((first, second ) => first + second)
+
+totalBatteries();
+    
